9727542910a6d28e87f6983a218b383324f0cd68
Thow-feeq <thowfiqahamed9@gmail.com>
2025-11-10 18:41:19 +0530

    updated backend controller

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { adminLogin, adminLogout, isAdminAuth } from '../controllers/adminController.js';
 import authAdmin from '../middlewares/authAdmin.js';
 
-const adminRouter = express.Router();
+const Router = express.Router();
 
 Router.post('/login', adminLogin);
 Router.get('/is-auth', authAdmin, isAdminAuth);
