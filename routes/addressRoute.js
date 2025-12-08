41039221cdc85c79d85e1589b45ff30c887fa29e
Thow-feeq <thowfiqahamed9@gmail.com>
2025-12-08 14:18:48 +0530

    ✅ Add category tree API for frontend

--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import authUser from '../middlewares/authUser.js';
-import { addAddress, getAddress } from '../controllers/addressController.js';
+import express from "express";
+import { addAddress, getAddress } from "../controllers/addressController.js";
+import authUser from "../middlewares/authMiddleware.js";
 
-const addressRouter = express.Router();
+const router = express.Router();
 
-addressRouter.post('/add', authUser, addAddress);
-addressRouter.get('/get', authUser, getAddress);
+router.post("/add", authUser, addAddress);
+router.get("/get", authUser, getAddress);
 
-export default addressRouter;
+export default router;
